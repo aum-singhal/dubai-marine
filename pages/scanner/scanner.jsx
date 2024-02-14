d384ce6c188d8e82ab27778cc15e876d072f11b1
Mac Os <macos@Macs-iMac-Pro.local>
2024-02-14 19:55:32 +0530

    switching to expo barcode scanner

--- pages/scanner/scanner.jsx
+++ pages/scanner/scanner.jsx
@@ -1,24 +1,27 @@
 import { Linking, View } from "react-native";
-import { CameraView, useCameraPermissions } from 'expo-camera/next';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function Scanner(){
-    const [facing, setFacing] = useState('back');
-    const [permission, requestPermission] = useCameraPermissions();
+    const [scanned, setScanned] = useState(false);
+    const [hasPermissions, setHasPermissions] = useState(null);
 
+    useEffect(()=>{
+        (async () =>{
+            const {status} = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermissions(status==='granted');
+        })();
+    }, [])
 
-    const HandleBarCodeScanned = (data) =>{
-        Linking.openURL(`${data.data}`);
+    const handleBarCodeScanned = ({type, data}) =>{
+        setScanned(true);
+        Linking.openURL(`${data}`);
     }
 
     return <View style={{flex: 1, flexDirection:"column"}}>
-        <CameraView 
-            onBarcodeScanned={HandleBarCodeScanned} 
-            barcodeScannerSettings={{
-                interval: 2,
-                barCodeTypes:['aztec','ean13','ean8','qr','pdf417','upc_e','datamatrix','code39','code93','itf14','codabar','code128','upc_a']
-            }}
-            style={{flex: 1}} >
-        </CameraView>
+        <BarCodeScanner 
+            onBarCodeScanned={scanned? undefined : handleBarCodeScanned}
+            style={{flex: 1}}
+        />
     </View>
 }
